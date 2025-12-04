e805681a30d9f38f1e2be6bd9b0caba1129be971
Fifid234 <amirnigmatullin57@gmail.com>
2025-12-04 22:38:54 +0500

    functions to operate with local storage

--- scrypts.js
+++ scrypts.js
@@ -1,5 +1,6 @@
 // Playlist {
 //    "Name": "",
+//    "Id": 0
 //    "Songs": []
 // }
 
@@ -8,40 +9,80 @@
 //     "Duration": 0,
 //     "Path": "",
 //     "Name": ""
+//     "Id": 0
 // }
 
-const MusicListView = document.getElementById("music-list-view");
-const LocalStorageKey = "Playlists";
+// b = [
+// new PlayList([
+//     new Song(
+//     12123,
+//     4,
+//     "erer",
+//     "Hell",
+//     131 
+// ), new Song(
+//     12123,
+//     4,
+//     "erer",
+//     "Hell",
+//     131 
+// ),
+// new Song(
+//     12123,
+//     4,
+//     "erer",
+//     "Hell",
+//     131 )
+// ], 123),
+// new PlayList([
+//     new Song(
+//     12123,
+//     4,
+//     "erer",
+//     "Hell",
+//     131 ), 
+// new Song(
+//     12123,
+//     4,
+//     "erer",
+//     "Hell",
+//     131 
+// ),
+// new Song(
+//     12123,
+//     4,
+//     "erer",
+//     "Hell",
+//     131 
+// )
+// ], 123), 
+// new PlayList([
+//     new Song(
+//     12123,
+//     4,
+//     "erer",
+//     "Hell",
+//     131 
+// ), new Song(
+//     12123,
+//     4,
+//     "erer",
+//     "Hell",
+//     131 
+// ),
+// new Song(
+//     12123,
+//     4,
+//     "erer",
+//     "Hell",
+//     131 
+// )
+// ], 123)]
 
-PlayLists = [];
-
-
-function GetPlaylists() {
-    PlaylistsDocument = localStorage.getItem(LocalStorageKey);
-
-    if (PlaylistsDocument === null) {
-        console.log("LocalStorageKey is Empty");
-
-        return null;
-    }
-
-    PlaylistsDocument = JSON.parse(PlaylistsDocument)
-
-    outPlayLists = []
-
-    for (let i = 0; i < PlaylistsDocument.length; i++) {
-        PlayListElement = PlaylistsDocument[i];
-
-        for (let j = 0; j < PlayListElement.length; j++) {
-            const element = array[];
-            
-        }
-    }
-}
 
 class PlayList {
-    Songs = [];
-    Id = 0;
+    Songs = null;
+    Id = null;
     constructor(Songs, Id) {
         this.Songs = Songs;
         this.Id = Id;
@@ -49,11 +90,11 @@ class PlayList {
 }
 
 class Song {
-    Date_Added = 0;
-    Duration = 0;
-    Path = "";
-    Name = "";
-    Id = 0;
+    Date_Added = null;
+    Duration = null;
+    Path = null;
+    Name = null;
+    Id = null;
     constructor(Date_Added, Duration, Path, Name, Id) {
         this.Date_Added = Date_Added;
         this.Duration = Duration;
@@ -62,3 +103,54 @@ class Song {
         this.Id = Id;
     }
 };
+
+
+const MusicListView = document.getElementById("music-list-view");
+const LocalStorageKey = "Playlists";
+
+let Playlists = GetPlaylists();
+
+SaveToLocalStorage();
+
+console.log(Playlists);
+
+function GetPlaylists() {
+    let localStorageData = JSON.parse(localStorage.getItem(LocalStorageKey));
+    let outPlayLists = [];
+
+    if (localStorageData === null) {
+        console.log("Local storage is", localStorageData);
+        
+        return null;
+    }
+
+    for (let i = 0; i < localStorageData.length - 1; i++) {
+        let rawSongs = localStorageData[i];
+        let songs = [];
+        
+        for (let j = 0; j < rawSongs.Songs.length - 1; j++) {
+            let rawSong = rawSongs.Songs[i];
+
+            let mainSong = new Song(
+                rawSong.Date_Added,
+                rawSong.Duration,
+                rawSong.Path,
+                rawSong.Name,
+                j
+            );
+            
+            songs.push(mainSong);
+        }
+
+        let mainPlayList = new PlayList(songs, i);
+        outPlayLists.push(mainPlayList);
+    }
+
+    return outPlayLists;
+}
+
+function SaveToLocalStorage() {
+    let strPlayLists = JSON.stringify(this.Playlists);
+
+    localStorage.setItem(LocalStorageKey, strPlayLists);
+}
